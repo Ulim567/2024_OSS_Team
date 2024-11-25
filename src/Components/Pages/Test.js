c43041d9f78ac97c8b36216631454dd763ecee05
Ulim567 <ulim040125@gmail.com>
2024-11-25 19:02:20 +0900

    complete setting

---
+++ src/Components/Pages/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export default function Test() {
+  return (
+    <>
+      <Button variant="primary">Test</Button>
+    </>
+  );
+}
